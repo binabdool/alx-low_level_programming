48a396cb35485ea50bc20cf98a534a2fcbb7b421
binabdool <seaneverdry877@gmail.com>
2023-03-02 05:11:58 -0800

    Numbers have life they are not just symbols on paper

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 
 /**
  * main - check the code
@@ -10,25 +7,15 @@
  */
 int main(void)
 {
-Print_number(98);
-printf("number %d is printed.", number);
-_putchar('\n');
-
-Print_number(402);
-
-_putchar('\n');
-
-Print_number(1024);
-
-_putchar('\n');
-
-Print_number(0);
-
-_putchar('\n');
-
-Print_number(-98);
-
-_putchar('\n');
-
-return (0);
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
