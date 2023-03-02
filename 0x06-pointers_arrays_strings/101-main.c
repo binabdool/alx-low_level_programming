32a59ce1a1ad32a326c33f2e68e4f4fc2319fb83
binabdool <seaneverdry877@gmail.com>
2023-03-02 03:36:59 -0800

    Magic

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * main - check the code
@@ -7,15 +10,25 @@
  */
 int main(void)
 {
-print_number(98);
+Print_number(98);
+printf("number %d is printed.", number);
 _putchar('\n');
-print_number(402);
+
+Print_number(402);
+
 _putchar('\n');
-print_number(1024);
+
+Print_number(1024);
+
 _putchar('\n');
-print_number(0);
+
+Print_number(0);
+
 _putchar('\n');
-print_number(-98);
+
+Print_number(-98);
+
 _putchar('\n');
+
 return (0);
 }
